e95b4b1e698943cdc8f6c16875805ed1ca524acc
Ankush Prajapati <139058537+ankushp965@users.noreply.github.com>
2024-11-30 20:16:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scrolling with slower speed
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      const targetElement = document.querySelector(this.getAttribute('href'));
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: 'smooth'
+      });
+    });
+  });
+  
